b0131b82e7d66aea85dba0031aba3af54d7f189a
Bidker <bida94@gmail.com>
2017-05-07 19:52:00 +0200

    add model and forms comment for the articles

--- articles/models.py
+++ articles/models.py
@@ -13,3 +13,11 @@ class Article(models.Model):
 	def __unicode__(self):
 		return self.title
 	
+class Comment(models.Model):
+	name = models.CharField(max_length = 150, verbose_name = "Użytkownik")
+	content = models.TextField(verbose_name = "Komentarz")
+	published = models.DateTimeField(verbose_name = "Data publikacji")
+	article = models.ForeignKey(Article)
+	
+	def __unicode__(self):
+		return self.name
