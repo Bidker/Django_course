b347ec59c6072c68b59379e2d7a25fc67fb25651
Bidker <bida94@gmail.com>
2017-05-09 16:47:04 +0200

    add template for CommentForm

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ from articles import views
 urlpatterns = [
 					  url(r'^show_all/$', views.articles, name = 'articles'),
 					  url(r'^(?P<pk>[0-9]+)/$', views.article, name = 'article'),
+					  url(r'^(?P<pk>[0-9]+)/add_comment/$', views.add_comment, name = 'add_comment'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render, redirect
+from .models import Article, Comment
+from forms import CommentForm
+from django.template.context_processors import csrf
+from django.utils import timezone
+from django.template.defaulttags import comment
 
 def articles(request):
     return render(request, 'articles.html', { 'articles' : Article.objects.all() })
 
 def article(request, pk):
     return render(request, 'article.html', { 'article' : Article.objects.get(pk = pk)} )
+
+def add_comment(request, pk):
+    art = Article.objects.get(pk = pk)
+    
+    if request.method == "POST":
+        cf = CommentForm(request.POST)
+        if cf.is_valid():
+            comment = cf.save(commit = False)
+            comment.published = timezone.now()
+            comment.article = art
+            comment.save()
+            
+            return redirect('article', pk=art.pk)
+    else:
+        cf = CommentForm()
+        
+    args = {}
+    args.update(csrf(request))
+    args['article'] = art
+    args['form'] = cf
+    
+    return render(request, 'add_comment.html/', args)
+
